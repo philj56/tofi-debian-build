f83b66ddcc97997234486f0ddd72f6ec47eb0166
Phil Jones <philj56@gmail.com>
2022-07-31 16:07:54 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# [Tofi](https://github.com/philj56/tofi) Debian tools
+This is a set of tools used to automatically generate a
+[tofi](https://github.com/philj56/tofi) `.deb` package for debian-like systems,
+specifically meant to run on Ubuntu 20.04. You really shouldn't try to run this
+on your machine, or if you do, at least do it in a VM.
---
+++ gendeb.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+
+version=0.3.1
+
+pushd .
+
+mkdir debbuild
+cd debbuild
+
+# curl -L https://github.com/philj56/tofi/archive/v$version/tofi-$version.tar.gz -o tofi_$version.orig.tar.gz 
+curl -L https://github.com/philj56/tofi/archive/refs/tags/ubuntu-packaging-2.tar.gz -o tofi_$version.orig.tar.gz 
+tar xf tofi_$version.orig.tar.gz
+mv tofi-ubuntu-packaging-2 tofi-$version
+cd tofi-$version
+
+cp -r ../../debian .
+cp LICENSE debian/copyright
+
+grep -rlI TOFI_VERSION debian | xargs sed -i "s/TOFI_VERSION/$version/g"
+
+debuild -us -uc
+
+popd
+
+mkdir -p tofi-debian
+cp debbuild/*.deb tofi-debian
